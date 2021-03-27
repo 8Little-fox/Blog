f3b85326faa11cf69460a330c8f651cfa7101e12
Lin <3462540450@qq.com>
2021-03-27 21:47:50 +0800

    update page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-27 21:28:29
+ * @LastEditTime: 2021-03-27 21:47:45
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \sll-blog\docs\.vuepress\config.js
+ */
 module.exports = {
+  base: '/blog/',
   themeConfig: {
     nav: [
       { text: '首页', link: '/' }
